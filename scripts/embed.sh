704436b20c72fb07eb178015e491676c9422261e
Jaeseok Lee <devsunb@gmail.com>
2025-02-13 14:45:51 +0900

    personal update
    remove lock

--- scripts/embed.sh
+++ scripts/embed.sh
@@ -4,31 +4,41 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/utils.sh"
 
 embed() {
-    unset_bindings
-    number_of_windows=$(tmux list-windows -t "$FLOAX_SESSION_NAME" | wc -l)
-    if [ "$number_of_windows" -eq 1 ]; then
-        # there's only one window, need to create an alternative
-        # before moving the current one to another session
-        # otherwise the session dies and popping back won't work
-        tmux neww -d
-    fi
-    tmux movew -t "$ORIGIN_SESSION"
+  number_of_windows=$(tmux list-windows -t "$FLOAX_SESSION_NAME" | wc -l)
+  move_window "$(tmux display -p '#{session_name}:#I')" "$ORIGIN_SESSION"
+  if [ "$number_of_windows" -ne 1 ]; then
     tmux detach-client
+  fi
 }
 
 pop() {
-    tmux movew -t "$FLOAX_SESSION_NAME"
-    tmux_popup
+  ORIGIN_SESSION="$(tmux display -p '#{session_name}')"
+  ORIGIN_WINDOW="$(tmux display -p '#I')"
+  create_session
+  move_window "$ORIGIN_SESSION:$ORIGIN_WINDOW" "$FLOAX_SESSION_NAME"
+  tmux setenv -g ORIGIN_SESSION "$ORIGIN_SESSION"
+  tmux_popup
+}
+
+embed_or_pop() {
+  if [ "$(tmux display-message -p '#{session_name}')" = "$FLOAX_SESSION_NAME" ]; then
+    embed
+  else
+    pop
+  fi
 }
 
 action=$1
 case "$action" in
-    embed)
-        embed
-        ;;
-    pop)
-        pop
-        ;;
+embed)
+  embed
+  ;;
+pop)
+  pop
+  ;;
+embed_or_pop)
+  embed_or_pop
+  ;;
 esac
 
 ORIGIN_SESSION="$(envvar_value ORIGIN_SESSION)"
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,113 +1,81 @@
 #!/usr/bin/env bash
 
 envvar_value() {
-    tmux showenv -g "$1" | cut -d '=' -f 2-
+  tmux showenv -g "$1" | cut -d '=' -f 2-
 }
 
 tmux_option_or_fallback() {
-	local option_value
-	option_value="$(tmux show-option -gqv "$1")"
-	if [ -z "$option_value" ]; then
-		option_value="$2"
-	fi
-	echo "$option_value"
+  local option_value
+  option_value="$(tmux show-option -gqv "$1")"
+  if [ -z "$option_value" ]; then
+    option_value="$2"
+  fi
+  echo "$option_value"
 }
 
+FLOAX_BORDER=$(envvar_value FLOAX_BORDER)
+FLOAX_BORDER_COLOR=$(envvar_value FLOAX_BORDER_COLOR)
+FLOAX_X=$(envvar_value FLOAX_X)
+FLOAX_Y=$(envvar_value FLOAX_Y)
 FLOAX_WIDTH=$(envvar_value FLOAX_WIDTH)
 FLOAX_HEIGHT=$(envvar_value FLOAX_HEIGHT)
-FLOAX_BORDER_COLOR=$(envvar_value FLOAX_BORDER_COLOR)
-FLOAX_TEXT_COLOR=$(envvar_value FLOAX_TEXT_COLOR)
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-FLOAX_CHANGE_PATH=$(envvar_value FLOAX_CHANGE_PATH)
 FLOAX_TITLE=$(envvar_value FLOAX_TITLE)
-DEFAULT_TITLE='FloaX: C-M-s 󰘕   C-M-b 󰁌   C-M-f 󰊓   C-M-r 󰑓   C-M-e 󱂬   C-M-d '
+DEFAULT_TITLE=''
 FLOAX_SESSION_NAME=$(envvar_value FLOAX_SESSION_NAME)
 DEFAULT_SESSION_NAME='scratch'
 
 set_bindings() {
-    tmux bind -n C-M-s run "$CURRENT_DIR/zoom-options.sh in"
-    tmux bind -n c-M-b run "$CURRENT_DIR/zoom-options.sh out"
-    tmux bind -n C-M-f run "$CURRENT_DIR/zoom-options.sh full"
-    tmux bind -n C-M-r run "$CURRENT_DIR/zoom-options.sh reset"
-    tmux bind -n C-M-e run "$CURRENT_DIR/embed.sh embed"
-    tmux bind -n C-M-d run "$CURRENT_DIR/zoom-options.sh lock" 
-    tmux bind -n C-M-u run "$CURRENT_DIR/zoom-options.sh unlock"
+  tmux bind C-M-e run "$CURRENT_DIR/embed.sh embed_or_pop"
+  tmux bind C-M-s run "$CURRENT_DIR/zoom-options.sh in"
+  tmux bind C-M-b run "$CURRENT_DIR/zoom-options.sh out"
+  tmux bind C-M-r run "$CURRENT_DIR/zoom-options.sh reset"
 }
 
 unset_bindings() {
-    tmux unbind -n C-M-s
-    tmux unbind -n C-M-b
-    tmux unbind -n C-M-f 
-    tmux unbind -n C-M-r 
-    tmux unbind -n C-M-e 
-    tmux unbind -n C-M-d 
-    tmux unbind -n C-M-u 
+  tmux unbind C-M-e
+  tmux unbind C-M-s
+  tmux unbind C-M-b
+  tmux unbind C-M-r
 }
 
-tmux_version() {
-    tmux -V | awk '{print $NF}' | sed 's/^[^0-9]*//'
+create_session() {
+  if ! tmux has-session -t "$FLOAX_SESSION_NAME" 2>/dev/null; then
+    tmux new-session -d -c "$(tmux display-message -p '#{pane_current_path}')" -s "$FLOAX_SESSION_NAME"
+  fi
 }
 
-# Checks whether tmux version is >= 3.3
-is_tmux_version_supported() {
-    local version
-    IFS='.' read -r -a version < <(tmux_version)
-
-    if [ "${version[0]}" -gt 3 ]; then
-        return 0
-    fi
-
-    # Minor version can be a number or alphanumeric, e.g. 3.3 vs 3.3a
-    if [ "${version[0]}" -eq 3 ] && [ "${version[1]//[!0-9]}" -ge 3 ]; then
-        return 0
-    fi
-
-    return 1
+move_window() {
+  FROM_WINDOW="$1"
+  TO_SESSION="$2"
+  TO_WIN_NUM="$(($(tmux list-windows -t "$TO_SESSION" | wc -l) + 1))"
+  tmux neww -t "$TO_SESSION:$TO_WIN_NUM"
+  tmux swapw -s "$FROM_WINDOW" -t "$TO_SESSION:$TO_WIN_NUM"
+  tmux killw -t "$FROM_WINDOW"
 }
 
 tmux_popup() {
-    # TODO: make this optional:
-    current_dir=$(tmux display -p '#{pane_current_path}')
-    scratch_path=$(tmux display -t scratch -p '#{pane_current_path}')
-    if [ "$scratch_path" != "$current_dir" ] && [ "$FLOAX_CHANGE_PATH" = "true" ]; then
-        tmux send-keys -R -t "$FLOAX_SESSION_NAME" " cd $current_dir" C-m
-    fi
+  FLOAX_WIDTH=$(envvar_value FLOAX_WIDTH)
+  FLOAX_HEIGHT=$(envvar_value FLOAX_HEIGHT)
 
-    if is_tmux_version_supported; then
-        if ! pop; then
-            tmux setenv -g FLOAX_WIDTH "$(tmux_option_or_fallback '@floax-width' '80%')" 
-            tmux setenv -g FLOAX_HEIGHT "$(tmux_option_or_fallback '@floax-height' '80%')"
-            pop
-        fi
-    else
-        tmux display-message \
-            -d 2000 \
-            "FloaX requires tmux version 3.3 or newer"
-    fi
-}
+  FLOAX_TITLE=$(envvar_value FLOAX_TITLE)
+  if [ -z "$FLOAX_TITLE" ]; then
+    FLOAX_TITLE="$DEFAULT_TITLE"
+  fi
 
-pop() {
-    FLOAX_WIDTH=$(envvar_value FLOAX_WIDTH)
-    FLOAX_HEIGHT=$(envvar_value FLOAX_HEIGHT)
+  FLOAX_SESSION_NAME=$(envvar_value FLOAX_SESSION_NAME)
+  if [ -z "$FLOAX_SESSION_NAME" ]; then
+    FLOAX_SESSION_NAME="$DEFAULT_SESSION_NAME"
+  fi
 
-    FLOAX_TITLE=$(envvar_value FLOAX_TITLE)
-    if [ -z "$FLOAX_TITLE" ]; then
-        FLOAX_TITLE="$DEFAULT_TITLE"
-    fi
-
-    FLOAX_SESSION_NAME=$(envvar_value FLOAX_SESSION_NAME)
-    if [ -z "$FLOAX_SESSION_NAME" ]; then
-        FLOAX_SESSION_NAME="$DEFAULT_SESSION_NAME"
-    fi
-
-    tmux set-option -t "$FLOAX_SESSION_NAME" detach-on-destroy on
-    tmux popup \
-        -S fg="$FLOAX_BORDER_COLOR" \
-        -s fg="$FLOAX_TEXT_COLOR" \
-        -T "$FLOAX_TITLE" \
-        -w "$FLOAX_WIDTH" \
-        -h "$FLOAX_HEIGHT" \
-        -b rounded \
-        -E \
-        "tmux attach-session -t \"$FLOAX_SESSION_NAME\"" 
+  tmux set-option -t "$FLOAX_SESSION_NAME" detach-on-destroy on
+  tmux popup \
+    -T "$FLOAX_TITLE" \
+    -x "$FLOAX_X" \
+    -y "$FLOAX_Y" \
+    -w "$FLOAX_WIDTH" \
+    -h "$FLOAX_HEIGHT" \
+    -b "$FLOAX_BORDER" \
+    -S fg="$FLOAX_BORDER_COLOR" \
+    -E "tmux attach-session -t \"$FLOAX_SESSION_NAME\""
 }
